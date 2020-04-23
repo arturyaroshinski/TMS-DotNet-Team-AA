9734aa9ce486bb8e3a9ca3ef3ef6b543eeb65cb2
Artur Yaroshinski <62154380+arturyaroshinski@users.noreply.github.com>
2020-04-24 01:25:33 +0300

    feat: added the ability to save the exchange rate, also made some refactoring

--- src/CurrencyExchange/CurrencyExchangeApp/Program.cs
+++ src/CurrencyExchange/CurrencyExchangeApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using CurrencyExchange.API;
 using Newtonsoft.Json;
@@ -11,6 +13,7 @@ namespace CurrencyExchangeApp
     {
         private static readonly HttpClient httpClient = new HttpClient();
 
+        private static readonly string path = @"..\test.txt";
 
         static void Main(string[] args)
         {
@@ -31,57 +34,67 @@ namespace CurrencyExchangeApp
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        GetAllCurrenciesAsync().GetAwaiter().GetResult();
+                        {
+                            ShowAllCurrencies();
+                        }
                         break;
                     case "2":
-                        GetRateAsync().GetAwaiter().GetResult();
-                        break;
+                        {
+                            GetRateAsync().GetAwaiter().GetResult();
+                            break;
+                        }
                     case "3":
-                        Environment.Exit(0);
-                        break;
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
                     default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Некорректный ввод.");
-                        break;
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Некорректный ввод.");
+                            break;
+                        }
                 }
             }
         }
 
-        // Вывод всех курсов на консоль.
-        private static async Task GetAllCurrenciesAsync()
+        // Возвращает массив объектов класса Currency.
+        private static async Task<Currency[]> GetAllCurrenciesAsync()
         {
             var response = await httpClient.GetAsync("https://www.nbrb.by/api/exrates/currencies");
 
             string content = await response.Content.ReadAsStringAsync();
             response.EnsureSuccessStatusCode();
 
-            List<Currency> currencies = JsonConvert.DeserializeObject<List<Currency>>(content);
-            foreach (var cur in currencies)
-            {
-                Console.WriteLine($"{cur.Cur_Name} : ID = {cur.Cur_ID}.");
-            }
+            return JsonConvert.DeserializeObject<Currency[]>(content);
         }
 
         // Вывод курса валюты по ID
         private static async Task GetRateAsync()
         {
-            // TODO: добавить проверку на существование такого id.
             int id = 298;
             bool flag = false;
 
             while (!flag)
             {
                 Console.WriteLine();
-                Console.WriteLine("Для получения курса введите ID валюты (Для просмотра списка валют введите 0).");
+                Console.WriteLine("Для получения курса введите ID валюты(Для просмотра списка валют введите 0).");
 
                 var userInput = Console.ReadLine();
 
-                if (userInput.Equals("0"))
-                    await GetAllCurrenciesAsync();
-                else
+                if (int.TryParse(userInput, out id))
                 {
-                    id = int.Parse(userInput);
-                    flag = true;
+                    if (id == 0)
+                        ShowAllCurrencies();
+                    else
+                    {
+                        if (IdIsExist(id))
+                            break;
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Валюты с ID {id} не существует.");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
                 }
             }
 
@@ -92,7 +105,45 @@ namespace CurrencyExchangeApp
 
             Rate rate = JsonConvert.DeserializeObject<Rate>(content);
 
-            Console.WriteLine($"{rate.Date}: {rate.Cur_Abbreviation}. Курс по НБРБ - {rate.Cur_OfficialRate}.");
+            var text = $"{rate.Date}: {rate.Cur_Abbreviation}. Курс по НБРБ - {rate.Cur_OfficialRate}.";
+            Console.WriteLine(text);
+
+            Console.WriteLine("Нажмите Y если хотите сохранить данные курса.");
+            if (Console.ReadKey().Key.Equals(ConsoleKey.Y))
+            {
+                await SaveAsync(path, text);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nСохранение выполнено успешно.");
+            }
+        }
+
+        /// <summary>
+        /// Вывод на консоль всех валют.
+        /// </summary>
+        public static void ShowAllCurrencies()
+        {
+            Currency[] currencies = GetAllCurrenciesAsync().GetAwaiter().GetResult();
+
+            foreach (var cur in currencies)
+            {
+                Console.WriteLine($"{cur.Cur_Name} : ID = {cur.Cur_ID}.");
+            }
+        }
+
+        // Возвращает true, если валюда с таким id существует.
+        private static bool IdIsExist(int id)
+        {
+            var currencies = GetAllCurrenciesAsync().GetAwaiter().GetResult();
+            return currencies.Any(x => x.Cur_ID == id);
+        }
+
+        // Сохранение строки text по пути path.
+        private static async Task SaveAsync(string path, string text)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true, Encoding.UTF8))
+            {
+                await sw.WriteLineAsync(text);
+            }
         }
     }
 }
